73187ef61f19434c40f87536be188fc80d0d1065
Marciel De Liz Santos <marcieldelizsantos@MacBook-Air-de-Marciel.local>
2020-05-27 22:09:30 -0300

    Hook useMemo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useMemo} from 'react';
 
 function App() {
-  const [tech, setTech] = useState([
-    'ReactJS',
-    'React Native'
-  ]);
+  const [tech, setTech] = useState([]);
   const [newTech, setNewTech] = useState('');
 
   function handleAdd(){
@@ -23,11 +20,14 @@ function App() {
     localStorage.setItem('tech', JSON.stringify(tech));
   },[tech]);
 
+  const techSize = useMemo(()=>tech.length, [tech]);
+
   return (
     <>
       <ul>
         {tech.map(t => <li key={t}>{t}</li>)}
       </ul>
+      <strong>Você tem {techSize} tecnologias</strong><br/>
       <input value={newTech} onChange={e => setNewTech(e.target.value)} />
       <button type="button" onClick={handleAdd}>Adicionar</button>
     </>
